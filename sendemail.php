196491f448d8f8f5fd725008d6999f23bc72a71f
AshishShadrakDeo <ashishshadrakdeo@.github.com>
2017-09-20 13:04:08 -0400

    Email Configuration

--- sendemail.php
+++ sendemail.php
@@ -6,7 +6,7 @@
     header( 'Content-type: application/json' );
 
 
-    $to         = 'trendytheme.net@gmail.com';  // put your email here
+    $to         = 's.ashish.d@gmail.com';  // put your email here
 
     $email_template = 'simple.html';
 
